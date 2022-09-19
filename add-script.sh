bcff948b16a325f927e9b4d2d6ba841a6213143f
Sola Otudeko <so2d@icloud.com>
2022-09-19 20:49:19 +0100

    1

---
+++ add-script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# A simple example script that publishes a number of scripts to the Nexus Repository Manager
+# and executes them.
+
+# fail if anything errors
+set -e
+# fail if a function call is missing an argument
+set -u
+
+username=admin
+password=admin123
+
+# add the context if you are not using the root context
+host=http://localhost:8081
+
+# add a script to the repository manager and run it
+name=$1
+file=$2
+
+printf "Provisioning Integration API Scripts Starting \n\n" 
+
+# using grape config that points to local Maven repo and Central Repository , default grape config fails on some downloads although artifacts are in Central
+# change the grapeConfig file to point to your repository manager, if you are already running one in your organization
+groovy -Dgroovy.grape.report.downloads=true -Dgrape.config=grapeConfig.xml addUpdateScript.groovy -u "$username" -p "$password" -n "$name" -f "$file" -h "$host"
+printf "\nPublished $file as $name\n\n"
+  
+
+
+# printf "Publishing and executing on $host\n"
+
+# addAndRunScript npmBower npmAndBowerRepositories.groovy
+# addAndRunScript raw rawRepositories.groovy
+# addAndRunScript security security.groovy
+# addAndRunScript core core.groovy
+
+# printf "\nProvisioning Scripts Completed\n\n"
+
+
