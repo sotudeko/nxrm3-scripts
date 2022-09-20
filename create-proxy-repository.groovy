9ab02cd750f87f6cef96f9a33d184828a02d5f27
Sola Otudeko <so2d@icloud.com>
2022-09-20 10:29:42 +0100

    5

--- create-proxy-repository.groovy
+++ create-proxy-repository.groovy
@@ -3,7 +3,12 @@ import org.sonatype.nexus.blobstore.api.BlobStoreManager
 //import org.sonatype.nexus.repository.storage.WritePolicy
 import org.sonatype.nexus.repository.maven.VersionPolicy
 import org.sonatype.nexus.repository.maven.LayoutPolicy
+import groovy.json.JsonSlurper
+ 
+//expects json string with appropriate content to be passed in
+def repodetails = new JsonSlurper().parseText(args)
 
 log.info("Create repository")
-repository.createMavenProxy('my-proxy-repo-3', 'https://www.proxythis-take2.com/', 'default', true, VersionPolicy.RELEASE, LayoutPolicy.PERMISSIVE)
+
+repository.createMavenProxy(repodetails.reponame, repodetails.url, 'default', true, VersionPolicy.RELEASE, LayoutPolicy.PERMISSIVE)
 
--- run-script.sh
+++ run-script.sh
@@ -14,8 +14,7 @@ host=${4:-http://localhost:8081}
 
 # add the context if you are not using the root context
 
-curl -v -X POST -u $username:$password --header "Content-Type: text/plain" "$host/service/rest/v1/script/${scriptname}/run"
-printf "\nSuccessfully executed ${scriptname} script\n\n\n"
+# curl -v -X POST -u $username:$password --header "Content-Type: text/plain" "$host/service/rest/v1/script/${scriptname}/run" -d '{"publicId": "MyApplicationID","name": "MyFirstApplication","organizationId":"f48b5344fa204a4c88df96b2d455d521","contactUserName":"AppContact","applicationTags": [{"tagId":"cd8fd2f4f289445b8975092e7d3045ba"}]}'
 
-
+curl -v -X POST -u $username:$password --header "Content-Type: text/plain" "$host/service/rest/v1/script/${scriptname}/run" -d '{"reponame": "the-proxy-repo","url": "http://www.bbc.co.uk"}'
 
